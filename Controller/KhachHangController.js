116cb04c27ed48b2db52fa37b93dc183843e2e38
Chikaturin <ninhhoaithanh@gmail.com>
2024-09-26 00:06:30 +0700

    finished

--- Controller/KhachHangController.js
+++ Controller/KhachHangController.js
@@ -3,7 +3,7 @@ const KhachHang = require("../Schema/schema.js").KhachHang;
 const GetKhachHang = async (req, res) => {
   try {
     const khachHang = await KhachHang.find({});
-    res.status(200).json({ khachHang });
+    res.status(200).json(khachHang);
   } catch (e) {
     res.status(500).json("not get khach hang");
   }
